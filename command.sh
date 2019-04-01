2d8d7e65d243f7058190376984abd692c9b51b1a
guotiexin@gmail.com <guotiexin@gmail.com>
2019-04-01 12:35:37 +0200

    first commit

---
+++ command.sh
@@ -0,0 +1,5 @@
+curl -H "Authorization: Bearer eyJrIjoidU42M21nMEpRaUlVMWZiekt5c0hwM0ZBTEZUZFJmdmMiLCJuIjoiQVBJIiwiaWQiOjF9" -H "Content-Type: application/json" -H "Accept: application/json" http://137.117.154.8/api/dashboards/db -d @create_dashboard_payload.json
+
+
+curl -H "Authorization: Bearer eyJrIjoidU42M21nMEpRaUlVMWZiekt5c0hwM0ZBTEZUZFJmdmMiLCJuIjoiQVBJIiwiaWQiOjF9" -H "Content-Type: application/json" -H "Accept: application/json" http://137.117.154.8/api/datasources -d @create_datasource_payload.json
+
